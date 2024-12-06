6e7d88bafb6b4d99321748beaaac399da5c5290a
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-06 09:28:41 -0800

    add GetChatMessagesHandler

--- api/handler/chat-handlers.go
+++ api/handler/chat-handlers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/JohnSalinas123/linguachat-backend-go/internal/database"
 	"github.com/gin-gonic/gin"
@@ -39,18 +40,53 @@ func GetChatsHandler(c *gin.Context) {
 
 }
 
-/*
+
 func GetChatMessagesHandler(c *gin.Context) {
 
 	// access database instance
 	db := database.GetPostgresConn()
 
-	userIDAny, exists := c.Get("userID")
-	if !exists {
-		log.Println("user_id missing")
+	chatId:= c.Param("chatID")
+	pageNum := c.Query("pageNum")
+	langCode := c.Query("langCode")
+
+	// format langCode into database compatible array
+	langCode = "{" + langCode + "}"
+
+	if len(langCode) == 0 {
+		log.Println("LangCode query parameter missing")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
-		return
 	}
 
+	if len(chatId) == 0 {
+		log.Println("ChatId path parameter missing")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+	}
+
+	if len(pageNum) == 0 {
+		log.Println("PageNum query parameter missing")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+	}
+
+	pageNumInt, err := strconv.Atoi(pageNum)
+	if err != nil {
+		log.Println("Failed to convert pageNum to int: %w", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+	}
+
+	if pageNumInt < 0 {
+		log.Println("Invalid messages page number")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+	}
+
+	messagesResponse, dbError := db.GetChatMessages(context.Background(),langCode, chatId, pageNumInt)
+	if dbError != nil {
+		log.Println("Failed to retrieve messages for chat: %w", dbError)
+		c.JSON(http.StatusInternalServerError, gin.H{"error":"Internal server error"})
+	}
+
+	log.Println(messagesResponse)
+
+	c.JSON(http.StatusOK, messagesResponse)
+
 }
-*/
