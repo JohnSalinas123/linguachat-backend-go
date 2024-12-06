e0b68812d57032cd9752032119e6bb1255b5d1a7
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-06 09:29:31 -0800

    add UpdateUserPublicData to set user language metadata primarily

--- internal/clerk/clerk.go
+++ internal/clerk/clerk.go
@@ -1,13 +1,18 @@
 package clerk
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 
 	clerkSDK "github.com/clerk/clerk-sdk-go/v2"
+	"github.com/clerk/clerk-sdk-go/v2/user"
 	svix "github.com/svix/svix-webhooks/go"
 )
 
 var webhookVerifier *svix.Webhook
+
 
 // intialClerkSetup returns error or nil
 // intial setup of clerk, sets secret key for session validation
@@ -32,4 +37,32 @@ func InitialClerkSetup(clerkSecret string, clerkWHSecret string) error {
 	webhookVerifier = wh
 
 	return nil
+}
+
+
+// updateUserPublicData updates a user's public data
+func UpdateUserPublicData(fieldName string, fieldValue string, userID string) error {
+
+	UserLangCode := map[string]interface{}{
+		fieldName : fieldValue,
+	}
+
+	publicMetadataJSON, err := json.Marshal(UserLangCode)
+	if err != nil {
+		return fmt.Errorf("error marshaling metadata")
+	}
+
+	rawMetadata := json.RawMessage(publicMetadataJSON)
+
+	user, err := user.UpdateMetadata(context.Background(), userID, &user.UpdateMetadataParams{
+		PublicMetadata: &rawMetadata,
+	})
+	if err != nil {
+		return fmt.Errorf("error updating metadata")
+	}
+
+	log.Printf("Successfully updated user metadata: %+v", user.PublicMetadata)
+
+	return nil
+
 }
