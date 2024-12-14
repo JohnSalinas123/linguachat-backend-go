7b90d83b3652cf4d28a85b04744855143ea7d1be
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-14 12:49:27 -0800

    add logging for token iat time for bugfixing wip, add leeway to token verification

--- internal/clerk/middleware.go
+++ internal/clerk/middleware.go
@@ -1,12 +1,14 @@
 package clerk
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
-	"github.com/clerk/clerk-sdk-go/v2"
-	clerkhttp "github.com/clerk/clerk-sdk-go/v2/http"
 	clerkJWT "github.com/clerk/clerk-sdk-go/v2/jwt"
 	"github.com/clerk/clerk-sdk-go/v2/user"
 	"github.com/gin-gonic/gin"
@@ -16,23 +18,82 @@ import (
 func ClerkAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		sessionToken := strings.TrimPrefix(c.Request.Header.Get("Authorization"), "Bearer ")
+		log.Println(sessionToken)
+
+		parts := strings.Split(sessionToken, ".")
+		
+		payload, err := base64.RawURLEncoding.DecodeString(parts[1])
+		
+		var claims map[string]interface{}
+		if err := json.Unmarshal(payload, &claims); err != nil {
+			log.Println("Failed to parse token payload", err)
+			return
+		}
+
+		if iat, ok := claims["iat"].(float64); ok {
+			issuedAt := time.Unix(int64(iat), 0)
+			loc, _ := time.LoadLocation("America/Los_Angeles")
+			currentTime := time.Now().In(loc)
+			log.Printf("Token issued at (iat): %v", issuedAt)
+			log.Printf("	     Current time: %v", currentTime)
+		} else {
+			log.Println("No 'iat' found in token payload")
+		}
+
+
+		/*
 		clerkhttp.RequireHeaderAuthorization()(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			c.Request = r
 		})).ServeHTTP(c.Writer, c.Request)
+		*/
 
+		/*
 		claims, ok := clerk.SessionClaimsFromContext(c.Request.Context())
+		log.Println("CLAIMS:", claims)
 		if !ok || claims.Subject == "" {
 			log.Println("Failed to retrieve session claims")
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			c.Abort()
 			return
 		}
+		*/
 
-		usr, err := user.Get(c.Request.Context(), claims.Subject)
+		claimsClerk, clerkErr := clerkJWT.Verify(c.Request.Context() , &clerkJWT.VerifyParams{
+			Token: sessionToken,
+			Leeway: 1*time.Second,
+			
+		})
+
+		log.Println("CLAIMS:", claimsClerk)
+ 
+		// parse and validate token
+		//log.Println(claims)
+		if clerkErr != nil {
+			log.Printf("Failed to parse or verify token %v", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"error":"Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		usr, err := user.Get(c.Request.Context(), claimsClerk.Subject)
+		if err != nil {
+			log.Printf("Failed to get 'sub' claim: %v", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"error":"Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		
+		/*
+		usr, err := user.Get(ctx, claims.Subject)
 		if err != nil {
 			log.Println("Failed to retrieve session subject claim")
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
 		}
+		*/
 
 		c.Set("userID", usr.ID)
 		c.Next()
